13e0c63c344f4c84721941acd0584700a578941e
Meghan Buchanan <meghan.buchanan@hotmail.com>
2024-11-06 21:16:52 -0800

    we can save now

--- something.js
+++ something.js
@@ -1,38 +1,64 @@
 const rows = 3;
 const columns = 2;
-let sur_la_time = 0;
-let sur_la_power = 0;
-let sur_la_money = 100;
-let sur_la_demand = 0;
-let sur_la_speedometer = 2;
-let sur_la_powerbalance = 0;
-let sur_la_fossil_fuel = 2000;
-let sur_la_fossil_fuel_plants = [
-  { powerOut: 30, fuelConsumption: 1, isOn: true },
-  { powerOut: 30, fuelConsumption: 1, isOn: true },
-  { powerOut: 30, fuelConsumption: 1, isOn: true },
-];
-let surLaSolarPanels = [
-  { powerOut: 10, cost: 20 },
-  { powerOut: 20, cost: 30 },
-  { powerOut: 40, cost: 40 },
-];
-let surLasur_la_TotalPower = 0;
+
+const sur = {
+    la: {
+        time: 0,
+        power: 0,
+        money: 100,
+        demand: 0,
+        speedometer: 2,
+        powerbalance: 0,
+        fossil_fuel: 2000,
+        fossil_fuel_plants: [
+            { powerOut: 30, fuelConsumption: 1, isOn: true },
+            { powerOut: 30, fuelConsumption: 1, isOn: true },
+            { powerOut: 30, fuelConsumption: 1, isOn: true },
+        ],
+        solarPanels: [
+            { powerOut: 10, cost: 20 },
+            { powerOut: 20, cost: 30 },
+            { powerOut: 40, cost: 40 },
+        ],
+        totalPower: 0
+    }
+}
+
+function loadData (jack) {
+    const load = JSON.parse(jack)
+    Object.assign(sur.la, load.la)
+}
+
+function saveData () {
+    const save = JSON.stringify(sur)
+    return save
+}
+
+document.getElementById("save").addEventListener("click", ()=>{
+    const jack = saveData()
+    document.getElementById("saveData").value = jack
+})
+
+document.getElementById("load").addEventListener("click", ()=>{
+    const newData = document.getElementById("saveData").value
+    loadData(newData)
+})
+
 setInterval(function () {
-  document.getElementById("time").innerText = "time: " + sur_la_time;
+  document.getElementById("time").innerText = "time: " + sur.la.time;
   document.getElementById("dolladollabillsyall").innerText =
-    "money: " + sur_la_money;
+    "money: " + sur.la.money;
   document.getElementById("itsover9000").innerText =
-    "power: " + surLasur_la_TotalPower;
+    "power: " + sur.la.totalPower;
   document.getElementById("acciopower").innerText =
-    "demand: " + sur_la_demand;
+    "demand: " + sur.la.demand;
   document.getElementById("fossilFuels:]").innerText =
-    "fossil fuels: " + sur_la_fossil_fuel;
-  let sous_la_hashtag = Array(Math.abs(Math.round(sur_la_powerbalance * 10)))
+    "fossil fuels: " + sur.la.fossil_fuel;
+  let sous_la_hashtag = Array(Math.abs(Math.round(sur.la.powerbalance * 10)))
     .fill("#")
     .join("");
   const demandometer = document.getElementById("demandometer");
-  if (sur_la_powerbalance >= 0) {
+  if (sur.la.powerbalance >= 0) {
     demandometer.innerText = "|" + sous_la_hashtag;
     demandometer.style.color = "green";
   } else {
@@ -43,24 +69,24 @@ setInterval(function () {
 setInterval(function () {
   let surLaDirtyPower = 0;
   let surLaDirtyPowerConsumption = 0;
-  for (laPlant of sur_la_fossil_fuel_plants) {
+  for (laPlant of sur.la.fossil_fuel_plants) {
     if (laPlant.isOn) {
       surLaDirtyPower += laPlant.powerOut;
       surLaDirtyPowerConsumption += laPlant.fuelConsumption;
     }
   }
-  surLasur_la_TotalPower = surLaDirtyPower + sur_la_power;
-  sur_la_time += sur_la_speedometer;
-  sur_la_money += Math.round(
-    sur_la_speedometer *
-      (Math.min(sur_la_demand, surLasur_la_TotalPower) / 60)
+  sur.la.totalPower = surLaDirtyPower + sur.la.power;
+  sur.la.time += sur.la.speedometer;
+  sur.la.money += Math.round(
+    sur.la.speedometer *
+      (Math.min(sur.la.demand, sur.la.totalPower) / 60)
   );
-  sur_la_demand += sur_la_speedometer * 10;
-  sur_la_powerbalance =
-    (surLasur_la_TotalPower - sur_la_demand) /
-    (surLasur_la_TotalPower + sur_la_demand);
-  sur_la_fossil_fuel = Math.max(
-    sur_la_fossil_fuel - surLaDirtyPowerConsumption * sur_la_speedometer,
+  sur.la.demand += sur.la.speedometer * 10;
+  sur.la.powerbalance =
+    (sur.la.totalPower - sur.la.demand) /
+    (sur.la.totalPower + sur.la.demand);
+  sur.la.fossil_fuel = Math.max(
+    sur.la.fossil_fuel - surLaDirtyPowerConsumption * sur.la.speedometer,
     0
   );
 }, 1000);
@@ -69,15 +95,15 @@ const sur_la_table = document.createElement("table");
 
 const solar_tr = document.createElement("tr");
 sur_la_table.appendChild(solar_tr);
-for (let i = 0; i < surLaSolarPanels.length; i++) {
-  const solarPanel = surLaSolarPanels[i];
+for (let i = 0; i < sur.la.solarPanels.length; i++) {
+  const solarPanel = sur.la.solarPanels[i];
   const td = document.createElement("td");
   td.style.backgroundColor = "lightBlue";
   td.innerText = `power: ${solarPanel.powerOut}  cost: ${solarPanel.cost}`;
   td.onclick = function () {
-    if (sur_la_money >= solarPanel.cost) {
-      sur_la_money = sur_la_money - solarPanel.cost;
-      sur_la_power = sur_la_power + solarPanel.powerOut;
+    if (sur.la.money >= solarPanel.cost) {
+      sur.la.money = sur.la.money - solarPanel.cost;
+      sur.la.power = sur.la.power + solarPanel.powerOut;
       solarPanel.cost = Math.round(solarPanel.cost * 1.05);
       td.innerText = `power: ${solarPanel.powerOut}  cost: ${solarPanel.cost}`;
     }
@@ -88,8 +114,8 @@ for (let i = 0; i < surLaSolarPanels.length; i++) {
 const soux_la_table = document.createElement("table");
 const tr = document.createElement("tr");
 soux_la_table.appendChild(tr);
-for (let i = 0; i < sur_la_fossil_fuel_plants.length; i++) {
-  const laPlant = sur_la_fossil_fuel_plants[i];
+for (let i = 0; i < sur.la.fossil_fuel_plants.length; i++) {
+  const laPlant = sur.la.fossil_fuel_plants[i];
   const td = document.createElement("td");
   td.style.backgroundColor = "lightGrey";
   td.innerText = `fossil fuel consumption = ${laPlant.fuelConsumption} power = ${laPlant.powerOut}`;
